799eecea95409a3e008a8098db43ed85cdef3dc4
Vladyslav Svetlov <svetlovevlad@gmail.com>
2024-09-04 21:21:52 +0200

    Pdf viewer

--- src/PdfViewer.js
+++ src/PdfViewer.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const PdfViewer = ({ pdfUrl }) => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <div className="pdf-viewer">
       <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.0.279/build/pdf.worker.min.js`}>
-        <Viewer fileUrl={pdfUrl} />
+        <Viewer fileUrl={pdfUrl} plugins={[defaultLayoutPluginInstance]}/>
       </Worker>
     </div>
   );
